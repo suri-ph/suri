7e240715f4db4bdaca730877be5ca29bef09bb16
johnraivenolazo@gmail.com <johnraivenolazo@gmail.com>
2025-11-02 13:37:37 +0800

    Adjust controlbar padding

--- desktop/src/components/main/components/ControlBar.tsx
+++ desktop/src/components/main/components/ControlBar.tsx
@@ -19,7 +19,7 @@ export function ControlBar({
 }: ControlBarProps) {
   return (
     <div>
-      <div className="rounded-lg p-5 pt-4.5 flex items-center justify-between min-h-[4rem]">
+      <div className="rounded-lg p-4 flex items-center justify-between min-h-[4rem]">
         <div className="flex items-center space-x-6">
           {/* Camera Selection */}
           {cameraDevices.length > 0 && (
